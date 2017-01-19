22ad6ce895ecedaaf2eab570d96bb06d1ce7ebf8
yutato40103 <yuito0627yy@docomo.ne.jp>
2017-01-20 00:55:56 +0900

    Add files via upload

---
+++ サルでもわかる.php
@@ -0,0 +1,11 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/wp32/sharereports/classes/Conf.php');
+
+session_destroy();
+
+header("Location: /wp32/sharereports/index.php");
+exit;
+
+?>
+kgpvekrpbkwag
+kebea
